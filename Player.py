5669e903591fd1ae84d927c7b98ce969cbe39d2e
beatriz <tris01140@gmail.com>
2024-11-22 22:30:10 -0400

    para que el programa espere un poco en responer

--- Player.py
+++ Player.py
@@ -1,40 +1,39 @@
-import math 
+import math
 import random
 
-
 class Player:
-    def _init_(self,letter):
-        #la letra es X u O
+    def __init__(self, letter):
+        # La letra es X u O
         self.letter = letter
 
     def get_move(self, game):
         pass
 
 class RandomComputerPlayer(Player):
-    def _init_(self, letter):
-        super()._init_(letter)
+    def __init__(self, letter):
+        super().__init__(letter)
 
     def get_move(self, game):
+        # Selecciona un movimiento aleatorio de las opciones disponibles
         square = random.choice(game.available_moves())
         return square
 
 class HumanPlayer(Player):
-    def _init_(self, letter):
-        super()._init_(letter)
+    def __init__(self, letter):
+        super().__init__(letter)
     
     def get_move(self, game):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. imput move (0-9):')
-            #tenemos que verificar que este es un valor correcto
-            #si el spot no esta disponible en el tablero, decimos que es invalido
+            square = input(self.letter + '\'s turn. Input move (0-8): ')
+            # Verificamos que el valor ingresado sea válido y el spot esté disponible
             try:
-                val= int(square)
+                val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
-                valid_square = True 
+                valid_square = True
             except ValueError:
-                print('invalid square, Try again.')
+                print('Invalid square. Try again.')
 
         return val
